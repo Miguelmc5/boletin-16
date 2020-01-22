ed77ad3b0bb0b5207962a963d7b5cf3c9bd220b9
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-22 10:59:03 +0100

    boletin 16

---
+++ boletin16/Boletin16.java
@@ -0,0 +1,20 @@
+package boletin16;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class Boletin16 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Canario cana = new Canario();
+        cana.cantar();
+
+        Tenor teno = new Tenor();
+        teno.cantar();    
+    
+    }
+    
+}
